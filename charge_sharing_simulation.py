9dd872cb069190a07252f6585f20de15042331ad
vGate <vgate@vGate-9.3>
2024-01-03 23:54:02 +0000

    python simulation 240103

--- charge_sharing_simulation.py
+++ charge_sharing_simulation.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import dblquad
+from scipy.integrate import quad
 from scipy import optimize
 from scipy import special
+from datetime import datetime
 
 def charge_sharing():
     print('Now we study charge-sharing')
@@ -63,14 +65,27 @@ def charge_sharing():
 def gauss2d(x,y,x0,y0,sigma,I):
     return I/(2*np.pi*np.power(sigma,2))*np.exp(-0.5*np.power(x-x0,2)/np.power(sigma,2)-0.5*np.power(y-y0,2)/np.power(sigma,2))
 
+def gauss2dwr(x,y,x0,y0,sigma,I,pp,array_s):
+    return (gauss2d(x,y,x0,y0,sigma,I) + 
+            gauss2d(x,y,x0+pp*array_s,y0,sigma,I) + 
+            gauss2d(x,y,x0+pp*array_s,y0+pp*array_s,sigma,I) +
+            gauss2d(x,y,x0,y0+pp*array_s,sigma,I)+
+            gauss2d(x,y,x0-pp*array_s,y0,sigma,I) + 
+            gauss2d(x,y,x0-pp*array_s,y0-pp*array_s,sigma,I) +
+            gauss2d(x,y,x0,y0-pp*array_s,sigma,I) )
+
 def gauss1d(x,x0,sigma,A):
     return A/(np.sqrt(2*np.pi)*sigma)*np.exp(-0.5*np.power(x-x0,2)/np.power(sigma,2))
 
 def box(x,x0,sigmac,Ac):
-    return Ac*special.erfc(np.sqrt(2)*(x-x0)/sigmac)
+    return Ac*special.erfc((x-x0)/(sigmac*np.sqrt(2)))
 
-def gauss_box(x,x0,sigma,A,Ac,An,x1,Ab):
-    return gauss1d(x,x0,sigma,A)+ box(x,x0,sigma,Ac) + gauss1d(x,0,sigma,An)+ gauss1d(x,x0+x1,sigma,Ab)
+def gauss_box(x,x0,sigma,A,Ac,x1,Ab):
+    return gauss1d(x,x0,sigma,A)+ box(x,x0,sigma,Ac) + gauss1d(x,x0+x1,sigma,Ab)
+
+def scurve_func(th, flex, noise, amplitude, chargesharing):
+    y=0.5*amplitude*(1+special.erf((flex-th)/(noise*np.sqrt(2))))*(1+chargesharing*(flex-th)/noise)
+    return y
 
 def Cu_Fluo():
     print('Now we study charge-sharing')
@@ -80,31 +95,33 @@ def Cu_Fluo():
     pp = 75 #micron
     array_s = 5
     nop = 1000
-    ev_mult = 1
-    Pgridx = np.random.uniform(0.2*pp,(array_s-.2)*pp, size=nop) # np.arange(-0.25*pp,+1.25*pp,0.1*pp)
-    Pgridy = np.random.uniform(0.2*pp,(array_s-.2)*pp, size=nop) #np.arange(-0.25*pp,+1.25*pp,0.1*pp)
-    # Pgridx = [pp/2] 
-    # Pgridy = [pp/2]
+    ev_mult = 10
+    Pgridx = np.random.uniform(0.*pp,(array_s-0.)*pp, size=nop) # np.arange(-0.25*pp,+1.25*pp,0.1*pp)
+    Pgridy = np.random.uniform(0.*pp,(array_s-0.)*pp, size=nop) #np.arange(-0.25*pp,+1.25*pp,0.1*pp)
+    # Pgridx = [1.1*pp] 
+    # Pgridy = [0.1*pp]
 
     print('Particles:', nop)
 
-    Evgridx = np.arange(-0.5*pp,(array_s+.5)*pp,0.01*pp)
-    Evgridy = np.arange(-0.5*pp,(array_s+.5)*pp,0.01*pp)
+    Evgridx = np.arange(0.*pp,(array_s+0.)*pp,0.01*pp)
+    Evgridy = np.arange(0.*pp,(array_s+0.)*pp,0.01*pp)
     X, Y = np.meshgrid(Evgridx, Evgridy)
 
     sigma = 7.5 # micron
-    sigmaeV = np.sqrt(np.power(50*3.6,2) + np.power(50,2)) # eV
+    sigmaeV = np.sqrt(np.power(45*3.6,2) + np.power(50,2)) # eV
     print('Energy resolution:', sigmaeV, 'eV')
     energy = np.array([8046 if np.random.uniform(0,1) > 0.12 else 8904 for i in range(nop)]) #Cu fluo 
     #energy = [6405 if np.random.uniform(0,1) > 0.12 else 7059 for i in range(nop)] #Fe fluo 
     charges = [[] for i in range(array_s*array_s)]
     cluster = []
 
-    print('Ka:',np.count_nonzero(energy == 8046)*ev_mult)
-    print('Kb:',np.count_nonzero(energy == 8904)*ev_mult)
+    num_ka = np.count_nonzero(energy == 8046)*ev_mult
+    num_kb = np.count_nonzero(energy == 8904)*ev_mult
+    print('Ka:',num_ka)
+    print('Kb:',num_kb)
 
-    thr0 = 8046/2
-    thr1 = (8046 + 8904)/2
+    thr0 = 7999.4/2
+    thr1 = 8000
     count0 = 0
     count1 = 0
     print('thr0:',thr0,'- thr2:',thr1)
@@ -117,19 +134,34 @@ def Cu_Fluo():
     x_ka_as_kb = []
     y_ka_as_kb = []
 
+    start = datetime.now()
+
     p_done = 0
     for x0,y0,e in zip(Pgridx,Pgridy,energy):
         # print('chcloud.shape:', np.shape(chcloud))
-        #print('coordinates:', x0,y0)
+        # print('coordinates:', x0,y0)
+        ipx = int(x0/pp) 
+        ipy = int(y0/pp)
+        # print(ipx,ipy)
+        itochx = [(ipx-1)%array_s,ipx%array_s,(ipx+1)%array_s]
+        itochy = [(ipy-1)%array_s,ipy%array_s,(ipy+1)%array_s]
+        # print('search in:', itochx,itochy)
 #        print('E:', e )
         for i in range(array_s*array_s):
             ix = i%array_s
             iy = int(i/array_s)
-            result, error = dblquad(gauss2d, 
-                            iy*pp,(iy+1)*pp,ix*pp,(ix+1)*pp, args=(x0,y0,sigma,e))
+            if ix in itochx and iy in itochy: 
+                if ix==array_s-1 or ix==0 or iy==array_s-1 or iy==0:     
+                    result, error = dblquad(gauss2dwr,
+                            iy*pp,(iy+1)*pp,ix*pp,(ix+1)*pp, args=(x0,y0,sigma,e,pp,array_s), epsabs=0.5)
+                else:
+                    result, error = dblquad(gauss2d,
+                            iy*pp,(iy+1)*pp,ix*pp,(ix+1)*pp, args=(x0,y0,sigma,e), epsabs=0.5)
+
+            else: result = 0
+#            print(ix,iy,'charges:',result)
             ch = np.random.normal(result, sigmaeV, ev_mult)
             charges[i].extend(ch)
-#            print(ix,iy,'charges:',result,ch)
             for ev in ch:
                 if ev > thr0 and ev <= thr1:
                     count0+=1
@@ -156,7 +188,13 @@ def Cu_Fluo():
         if np.random.uniform(0,1) > -0.05: continue
         sub1.cla()
         sub2.cla()
-        chcloud = gauss2d(X,Y,x0,y0,sigma,1)
+        chcloud = (gauss2d(X,Y,x0,y0,sigma,1) + 
+                    gauss2d(X,Y,x0+pp*array_s,y0,sigma,1) + 
+                    gauss2d(X,Y,x0+pp*array_s,y0+pp*array_s,sigma,1) +
+                    gauss2d(X,Y,x0,y0+pp*array_s,sigma,1)+
+                    gauss2d(X,Y,x0-pp*array_s,y0,sigma,1) + 
+                    gauss2d(X,Y,x0-pp*array_s,y0-pp*array_s,sigma,1) +
+                    gauss2d(X,Y,x0,y0-pp*array_s,sigma,1)                    )
         sub1.contourf(X,Y,chcloud)
         sub1.plot(x0,y0, '.')
         for i in range(array_s*array_s):
@@ -173,13 +211,18 @@ def Cu_Fluo():
         fig1.show()
         plt.pause(0.1)
 
-    print('Counter0:', count0)
-    print('Counter1:', count1)
+    now = datetime.now()
+    elapsed_time = now-start
+    print('Elapsed time:', str(elapsed_time))
 
-    print('Ka corr:', np.shape(x_ka_corr))
-    print('Kb corr:', np.shape(x_kb_corr))
-    print('Kb as Ka:', np.shape(x_kb_as_ka))
-    print('Ka as Kb:', np.shape(x_ka_as_kb))
+    print('Counter0:', count0, '/', num_ka, ' :', count0/num_ka)
+    print('Counter1:', count1, '/', num_kb, ' :', count1/num_kb)
+
+    print('Ka corr:', np.shape(x_ka_corr)[0], '/', num_ka, ' :', np.shape(x_ka_corr)[0]/num_ka)
+    print('Kb corr:', np.shape(x_kb_corr)[0], '/', num_kb, ' :', np.shape(x_kb_corr)[0]/num_kb)
+    print('Kb as Ka:', np.shape(x_kb_as_ka)[0], '/', num_kb, ' :', np.shape(x_kb_as_ka)[0]/num_kb)
+    print('Ka as Kb:', np.shape(x_ka_as_kb)[0], '/', num_ka, ' :', np.shape(x_ka_as_kb)[0]/num_ka)
+    print('Correct counts (ka_corr/count0):', np.shape(x_ka_corr)[0]/count0)
 
     bins = 20
     fig4, sub4 = plt.subplots() #ka_corr
@@ -207,9 +250,21 @@ def Cu_Fluo():
         # sub2.plot(b[1:], gauss1d(b[1:],param[0],param[1],param[2]),':')
         # sub2.plot(b[1:], box(b[1:],param[0],param[1],param[3]),':')
 
-    na,ba,pa = sub2.hist(np.array(charges).flatten(), bins=200, range=(0*np.min(energy),1.5*np.max(energy)),histtype='step', label='average')
+    na,ba,pa = sub2.hist(np.array(charges).flatten(), bins=400, range=(0*np.min(energy),1.5*np.max(energy)),histtype='step', label='average')
     scurve = [np.sum(na[i:]) for i in range(len(na))]
     sub3.plot(ba[1:],scurve)
+
+    xmin = 100
+    paramscurve, cova = optimize.curve_fit(scurve_func,ba[xmin+1:],scurve[xmin:],
+                        p0=[8046, sigmaeV, nop, 0.15])
+    sub3.plot(ba,scurve_func(ba,paramscurve[0],paramscurve[1],paramscurve[2],paramscurve[3]),':')
+    print('flex:', paramscurve[0])
+    print('sigma:', paramscurve[1])
+    print('Chsharing:', paramscurve[3])
+    sub3.set_xlabel('Energy (eV)')
+    sub3.set_ylabel('Counts')
+    fig3.show()
+
     #cluster = np.sum(charges,axis=0)
     cluster = []
     for i in range(array_s-1):
@@ -221,19 +276,23 @@ def Cu_Fluo():
             
     # print('Charges:', np.shape(charges))
     # print('Cluster:', np.shape(cluster))
-    nc,bc,pc = sub2.hist(cluster, bins=200, range=(0*np.min(energy),1.5*np.max(energy)),histtype='step', label='clusters')
+    nc,bc,pc = sub2.hist(cluster, bins=400, range=(0*np.min(energy),1.5*np.max(energy)),histtype='step', label='clusters')
 
-    parama, cova = optimize.curve_fit(gauss_box,ba[1:],na,
-                        p0=[np.min(energy),sigmaeV,nop,nop/2,nop,1000,nop],
-                        bounds=[[0,0,0,0,0,0,0],[9000,1e3,1e6,1e6,1e6,1500,1e6]])
-    print(parama)
-    sub2.plot(ba[1:], gauss_box(ba[1:],parama[0],parama[1],parama[2],parama[3],parama[4],parama[5],parama[6]),'-')
+    parama, cova = optimize.curve_fit(gauss_box,ba[xmin+1:],na[xmin:],
+                        p0=[8046,sigmaeV,nop,10,1000,nop/5],
+                        bounds=[[0,0,0,0,500,0],[9000,1e3,1e6,1e6,1500,1e6]])
+    print('X0:', parama[0])
+    print('sigma:', parama[1])
+    print('X1:', parama[0]+parama[4])
+    print('CS:', parama[3])
 
-    paramc, covc = optimize.curve_fit(gauss_box,bc[1:],nc,
-                        p0=[np.min(energy),sigmaeV,nop,nop/2,nop,1000,nop],
-                        bounds=[[0,0,0,0,0,0,0],[9000,1e3,1e6,1e6,1e6,1500,1e6]])
-    print(paramc)
-    sub2.plot(bc[1:], gauss_box(bc[1:],paramc[0],paramc[1],paramc[2],paramc[3],paramc[4],paramc[5],paramc[6]),'-')
+    sub2.plot(ba[1:], gauss_box(ba[1:],parama[0],parama[1],parama[2],parama[3],parama[4],parama[5]),'-')
+
+    paramc, covc = optimize.curve_fit(gauss_box,bc[xmin+1:],nc[xmin:],
+                        p0=[8046,sigmaeV,nop,10,1000,nop/5],
+                        bounds=[[0,0,0,0,500,0],[9000,1e3,1e6,1e6,1500,1e6]])
+    # print(paramc)
+    sub2.plot(bc[1:], gauss_box(bc[1:],paramc[0],paramc[1],paramc[2],paramc[3],paramc[4],paramc[5]),'-')
     sub2.plot([thr0,thr0],[0,np.max(nc)],':')
     sub2.plot([thr1,thr1],[0,np.max(nc)],':')
 
@@ -241,9 +300,44 @@ def Cu_Fluo():
     fig1.show()
     fig2.show()
 
-    sub3.set_xlabel('Energy (eV)')
-    sub3.set_ylabel('Counts')
-    fig3.show()
-
     fig5, sub5 = plt.subplots() #kb_corr
+
+    sub5.scatter(x_kb_corr,y_kb_corr,alpha=0.1, marker='.', color='blue', label='Kb correct')
+    sub5.scatter(x_kb_corr,pp-np.array(y_kb_corr),alpha=0.1, marker='.', color='blue')
+    sub5.scatter(pp-np.array(x_kb_corr),pp-np.array(y_kb_corr),alpha=0.1, marker='.', color='blue')
+    sub5.scatter(pp-np.array(x_kb_corr),np.array(y_kb_corr),alpha=0.1, marker='.', color='blue')
+    sub5.scatter(x_kb_as_ka,y_kb_as_ka,alpha=0.1, marker='.', color='red', label='Kb as Ka')
+    sub5.scatter(x_kb_as_ka,pp-np.array(y_kb_as_ka),alpha=0.1, marker='.', color='red')
+    sub5.scatter(pp-np.array(x_kb_as_ka),y_kb_as_ka,alpha=0.1, marker='.', color='red')
+    sub5.scatter(pp-np.array(x_kb_as_ka),pp-np.array(y_kb_as_ka),alpha=0.1, marker='.', color='red')
+    sub5.plot([0,pp,pp,0,0],[0,0,pp,pp,0],'-')
+    sub5.set_xlabel('x(um)')
+    sub5.set_ylabel('y(um)')
+    sub5.set_xlim([-0.1*pp,1.1*pp])
+    sub5.set_ylim([-0.1*pp,1.1*pp])
+    leg = sub5.legend()
+    for lh in leg.legendHandles: 
+        lh.set_alpha(1)
+    fig5.show()
+
     fig6, sub6 = plt.subplots() #kb_as_ka
+    sub6.set_xlabel('x(um)')
+    sub6.set_ylabel('y(um)')
+    # fig6.show()
+
+def use_modulus():
+    fig5, sub5 = plt.subplots() #kb_corr
+    x0 = 10
+    s = 10
+    A = 1
+    x = np.arange(-75,3*75,0.1)
+    sub5.plot(x%150,gauss1d(x,x0,s,A))
+
+    result, error = quad(gauss1d, 
+                      75,150, args=(x0,s,A))
+    result1, error = quad(gauss1d, 
+                      0,75, args=(x0,s,A))
+
+    print(result)
+    print(result1)
+    fig5.show()
